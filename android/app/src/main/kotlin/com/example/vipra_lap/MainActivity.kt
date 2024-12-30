d72f24d0bff4b2ef5f304980c92a41e7d8c2ad4a
rahul <rkrahulroy151617@gmail.com>
2024-12-30 18:06:39 +0530

    project create || app icon change || Splash screen create

---
+++ android/app/src/main/kotlin/com/example/vipra_lap/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.vipra_lap
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screen/start_screen/splash_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            /// Lottie Animation
+            Lottie.asset("assets/lottie/heart beat animation.json",
+                height: 300, width: 300)
+          ],
+        ),
+      ),
+    );
+  }
+}
