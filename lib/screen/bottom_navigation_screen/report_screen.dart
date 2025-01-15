69a2c502d3a914cc2793b854b658d4a4df5b35ff
rahul <rkrahulroy151617@gmail.com>
2025-01-15 15:10:29 +0530

    Report Screen Complete

--- lib/screen/bottom_navigation_screen/report_screen.dart
+++ lib/screen/bottom_navigation_screen/report_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vipra_lap/domain/utils/custom_text_style.dart';
 
 class ReportScreen extends StatefulWidget {
   const ReportScreen({super.key});
@@ -10,6 +11,31 @@ class ReportScreen extends StatefulWidget {
 class _ReportScreenState extends State<ReportScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold() ;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          "Report",
+          style: myTextStyle24(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/icons/report-data.png",
+              height: 100,
+              width: 100,
+              color: Colors.black45,
+            ),
+            const SizedBox(height: 6,),
+            Text("No Report Found" , style: myTextStyle18(fontWeight: FontWeight.bold),)
+          ],
+        ),
+      ),
+    );
   }
 }
